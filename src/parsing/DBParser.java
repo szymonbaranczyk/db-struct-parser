165aac7e90c048e34c6ed47eaa1f036e5f075b8e
Szymon Barańczyk <szymon.baranczyk@gmail.com>
2015-03-20 20:45:13 +0100

    Adding comments to DBParser

--- src/parsing/DBParser.java
+++ src/parsing/DBParser.java
@@ -14,28 +14,40 @@ import java.util.Scanner;
  */
 public class DBParser {
 
-
+    /**
+     *
+     * @param line
+     * @return  substring between first and last "`" in the line (exclusive)
+     */
     private static String getName(String line)
     {
         return line.substring(line.indexOf("`")+1,line.lastIndexOf("`"));
     }
+
+    /**
+     *
+     * @param line
+     * @return substring between:
+     *      -point that is 2 chars after last "`" in the line
+     *      -first " " AFTER this point
+     */
     private static String getType(String line)
     {
         return line.substring(line.lastIndexOf("`")+2,line.indexOf(" ",line.lastIndexOf("`")+2));
     }
     public static List<Table> parse(File file) throws FileNotFoundException {
         final Scanner sc=new Scanner(file);
-        ArrayList<Table> tables=new ArrayList<Table>();
-        while (sc.hasNextLine()) {
-            String lineFromFile = sc.nextLine();
-            if (lineFromFile.contains("CREATE TABLE")) {
-                Table table = new Table(getName(lineFromFile));
-                tables.add(table);
-                lineFromFile = sc.nextLine();
-                while (lineFromFile.charAt(2) == '`') {
-                    Column column = new Column(getName(lineFromFile), getType(lineFromFile));
-                    table.columns.add(column);
-                    lineFromFile = sc.nextLine();
+        ArrayList<Table> tables=new ArrayList<Table>();                 //create returned table
+        while (sc.hasNextLine()) {                                      //gets through whole file line by line
+            String lineFromFile = sc.nextLine();                        //jump to next line
+            if (lineFromFile.contains("CREATE TABLE")) {                //find CREATE TABLE statement
+                Table table = new Table(getName(lineFromFile));         //get name from line
+                tables.add(table);                                      //add current table to final list
+                lineFromFile = sc.nextLine();                           //move inside CREATE TABLE statement
+                while (lineFromFile.charAt(2) == '`') {                 //continues until you find line not starting with name, i. e. key declaration
+                    Column column = new Column(getName(lineFromFile), getType(lineFromFile));   //get necessary info
+                    table.columns.add(column);                          //add to results
+                    lineFromFile = sc.nextLine();                       //jump to next line
                 }
             }
 
